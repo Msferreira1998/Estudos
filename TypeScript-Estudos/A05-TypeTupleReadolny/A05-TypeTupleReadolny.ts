55aff68ccc7a22c136b9b7304dfaa8d837e163c2
Matheus <msferreira1998@gmail.com>
2021-07-19 15:45:46 -0300

    Tuple & readonly

---
+++ TypeScript-Estudos/A05-TypeTupleReadolny/A05-TypeTupleReadolny.ts
@@ -0,0 +1,6 @@
+const dados : readonly [number, string] = [1, 'Matheus'];
+const dados2 : [number, string?] = [1];
+const dados3 : [number, ...string[]] = [1, 'Matheus', 'outro'];
+
+const array1: readonly string[] = ['mar'];
+const array2: ReadonlyArray<string> = ['mar'];
