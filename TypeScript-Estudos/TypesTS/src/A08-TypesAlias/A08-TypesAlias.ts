d0afd317a92e4c23b2893268b116049e1b36b6f3
Matheus <msferreira1998@gmail.com>
2021-07-21 14:46:01 -0300

    type alias

--- TypeScript-Estudos/TypesTS/src/A08-TypesAlias/A08-TypesAlias.ts
+++ TypeScript-Estudos/TypesTS/src/A08-TypesAlias/A08-TypesAlias.ts
@@ -1,1 +1,25 @@
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: string;
+};
 
+type Cores = 'Vermelho' | 'Azul' | 'Amarelo' | 'Verde';
+type Cores2 = 'Rosa' | 'Branco' | 'Roxo';
+type CorPreferida = Cores | Cores2;
+
+const pessoa: Pessoa = {
+  idade: 30,
+  nome: 'Matheus',
+  salario: 200000,
+};
+
+function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+setCorPreferida(pessoa, 'Roxo');
+
+export default 1;
