db1996fe70f1e7f9a290324647811608cc6aca4d
Matheus <msferreira1998@gmail.com>
2021-07-25 11:56:44 -0300

    overload

---
+++ TypeScript-Estudos/TypesTS/src/A18-Overload/A18-Overload.ts
@@ -0,0 +1,23 @@
+type Adder = {
+  (x: number): number;
+  (x: number, y: number): number;
+  (...arg: number[]): number;
+};
+
+const adder: Adder = (x: number, y?: number, ...args: number[]) => {
+  if (args.length > 0)
+    return (
+      args.reduce((s, v) => {
+        return s + v;
+      }, 0) +
+      x +
+      (y || 0)
+    );
+  return x + (y || 0);
+};
+
+console.log(adder(1));
+console.log(adder(1, 2));
+console.log(adder(1, 2, 3));
+
+export default 1;
