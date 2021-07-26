5a116637dd744bdf38a5b5d83d5a286a63e194ce
Matheus <msferreira1998@gmail.com>
2021-07-26 15:08:53 -0300

    Exercicio votação

--- TypeScript-Estudos/Exercício2/src/votacao.ts
+++ TypeScript-Estudos/Exercício2/src/votacao.ts
@@ -1,3 +1,4 @@
+// Tentativa 1
 class VotoProgramacao {
   static votoInicial = 0;
 
@@ -43,3 +44,54 @@ votoProgramacao.votoTS();
 votoProgramacao.votoTS();
 
 votoProgramacao.votos();
+
+// Tentativa 2
+
+// type VoteOption = {
+//   option: string;
+//   numVotos: number;
+// };
+
+// class VotationOptions {
+//   private _votationOption: VoteOption[] = [];
+
+//   constructor(public details: string) {}
+
+//   addOption(voteOption: VoteOption) {
+//     this.votationOption.push(voteOption);
+//   }
+
+//   vote(indexOption: number): void {
+//     if (!this.votationOption[indexOption]) return;
+//     this.votationOption[indexOption].numVotos += 1;
+//   }
+
+//   get votationOption() {
+//     return this._votationOption;
+//   }
+// }
+
+// class ResultVotation {
+//   votations: VotationOptions[] = [];
+
+//   addVote(votation: VotationOptions) {
+//     this.votations.push(votation);
+//   }
+
+//   showVotes() {
+//     for (const votation of this.votations) {
+//       console.log(votation.details);
+//       for (const votationOption of votation.votationOption)
+//         console.log(votationOption.option, votationOption.numVotos);
+//     }
+//   }
+// }
+
+// const voto1 = new VotationOptions('Qual a sua cor favorita');
+// voto1.addOption({ option: 'Azul', numVotos: 0 });
+// voto1.addOption({ option: 'vermelho', numVotos: 0 });
+// voto1.vote(0);
+
+// const votationApp = new ResultVotation();
+// votationApp.addVote(voto1);
+// votationApp.showVotes();
