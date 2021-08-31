9eac59a7d1197e813630e641fe6bb85beaf473a6
Matheus Ferreira <msferreira1998@gmail.com>
2021-08-31 14:20:36 -0300

    initial configs

---
+++ next-graphql/src/pages/index.tsx
@@ -0,0 +1,18 @@
+export default function hora() {
+    const olaMundo = 'Hello world!!';
+    return (
+        <html lang="pt-BR">
+            <head>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <title>Next Project Study</title>
+            </head>
+            <body>
+                <h1>{olaMundo}</h1>
+            </body>
+        </html>
+    );
+}
