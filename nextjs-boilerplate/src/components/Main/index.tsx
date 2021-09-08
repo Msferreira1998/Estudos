fc3c0e947943e824306cdb58d1a055f790c06e1f
Matheus Ferreira <msferreira1998@gmail.com>
2021-09-08 10:52:46 -0300

    create boilerplate NextJs with jest, styled components and babel

--- nextjs-boilerplate/src/components/Main/index.tsx
+++ nextjs-boilerplate/src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-export default function Main() {
+import * as S from './styles';
+
+export default function Main({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/images/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/images/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
   );
 }
