e3a3acd5f9bcaaaa3a72d41410c3d4a169abe5a7
Matheus Ferreira <msferreira1998@gmail.com>
2021-09-14 13:25:04 -0300

    page about and component/about Template

---
+++ nextjs-graphql/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AboutTemplate from 'Templates/About';
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <AboutTemplate />
+    </React.Fragment>
+  );
+}
