273069f58f864adf4c9066459e39ebb7e56f911c
Matheus Ferreira <msferreira1998@gmail.com>
2021-09-20 16:01:42 -0300

    graphql request finally worked :')

--- nextjs-graphql/src/pages/About.tsx
+++ nextjs-graphql/src/pages/About.tsx
@@ -1,6 +1,17 @@
+import client from 'graphql/client';
+import { GET_PAGES } from 'graphql/queries';
 import React from 'react';
 import AboutTemplate from 'Templates/About';
 
 export default function AboutPage() {
   return <AboutTemplate />;
 }
+
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES);
+  console.log(pages);
+
+  return {
+    props: {},
+  };
+};
