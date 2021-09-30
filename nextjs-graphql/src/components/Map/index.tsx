ebd91cd293cfbadfb6faa255a9db685eff5c9de6
Matheus Ferreira <70543896+Msferreira1998@users.noreply.github.com>
2021-09-30 11:17:28 -0300

    Update index.tsx

--- nextjs-graphql/src/components/Map/index.tsx
+++ nextjs-graphql/src/components/Map/index.tsx
@@ -42,6 +42,8 @@ export default function Map({ places }: MapProps) {
       center={[0, 0]}
       zoom={3}
       style={{ height: '100%', width: '100%' }}
+      minZoom={3}
+      maxBounds={[[180, -180], [-180, 180]]}
     >
       <CustomTileLayer />
       {places?.map(({ id, name, slug, location }) => {
