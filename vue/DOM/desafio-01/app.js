a99d0ee72bbf253c042767c4ae2aa1833bdd0af5
Matheus Ferreira <msferreira1998@gmail.com>
2021-10-01 13:24:33 -0300

    Atividade 1 completa

---
+++ vue/DOM/desafio-01/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#desafio',
+    data:{
+        SEU_NOME: 'Matheus Ferreira',
+        IDADE: 23,
+        jojoImg: 'https://i.ytimg.com/vi/1hclmYoxgg0/maxresdefault.jpg'
+    },
+    methods:{
+        idadeX3() {
+            return (this.IDADE * 3)
+        },
+
+        RandomNum: () => Math.random()
+    }
+})
