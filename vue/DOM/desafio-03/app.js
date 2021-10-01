e9753ae024fb4a04830ad400b6840ee78ed976c0
Matheus Ferreira <msferreira1998@gmail.com>
2021-10-01 17:13:03 -0300

    Atividade 3

--- vue/DOM/desafio-03/app.js
+++ vue/DOM/desafio-03/app.js
@@ -2,18 +2,21 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0,
-        resultado: ''
+        // resultado: ''
+    },
+    computed: {
+        resultado(){
+            return this.valor !== 37 ? 'Valor Diferente' : 'Valor Igual'
+        }
     },
     watch:{
-        valor(){
-            this.resultado = this.valor !== 37 ? 'Valor Diferente' : 'Valor Igual'
-        },
+        // valor(){
+        //     this.resultado = this.valor !== 37 ? 'Valor Diferente' : 'Valor Igual'
+        // },
         resultado(){
-            
-            setTimeout(()=>{
-                this.valor = 0;
-            }, 5000)
-            
+                setTimeout(()=>{
+                    this.valor = 0;
+                }, 5000);          
         }
     }
 });
