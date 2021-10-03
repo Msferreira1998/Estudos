7588d59c64bf40d06d82d2c1ae8efa8f8ac97f87
Matheus Ferreira <msferreira1998@gmail.com>
2021-10-02 22:01:09 -0300

    vue/cli

---
+++ vue/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <button @click="titulo += '#'">Add #</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Olá Mundo'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    background-color: chocolate;
+    color: white;
+  }
+</style>
