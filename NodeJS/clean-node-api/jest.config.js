ecf1762fee12783cdff2ce7c02147495742fa3e6
Matheus Ferreira <msferreira1998@gmail.com>
2022-04-06 16:07:20 -0300

    chore: add jest

---
+++ NodeJS/clean-node-api/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
