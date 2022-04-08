bdab1de814a2b62c22b0db85ac7f0083816ab8d6
Matheus Ferreira <msferreira1998@gmail.com>
2022-04-08 15:49:03 -0300

    chore: aulas de typescript basico

---
+++ typeScript/Ts-basico-ao-avancado/1_intro/index.ts
@@ -0,0 +1,13 @@
+const firstName = "Matheus"
+const anotherName = 1
+const x = true
+
+function greeting(name :string) {
+    console.log(name)
+}
+
+greeting(firstName)
+// greeting(anotherName)
+// greeting(x)
+
+export default 1
---
+++ typeScript/Ts-basico-ao-avancado/3_tipos/exercicio.ts
@@ -0,0 +1,7 @@
+export default 1
+
+const num = 12
+const str: string = num.toString()
+const frase = `O numero é ${str}`
+
+console.log(frase)
